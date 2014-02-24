0d846e7762c008efef4ef55591d3974c183c134e
Christopher Hallahan <challahan@gmail.com>
2014-02-24 01:58:46 -0500

    Grunt fun added
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,9 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+  
     pkg: grunt.file.readJSON('package.json'),
 
+// SASS //
     sass: {
       options: {
         includePaths: ['bower_components/foundation/scss']
@@ -15,7 +17,24 @@ module.exports = function(grunt) {
         }        
       }
     },
+//End SASS //
 
+// Autoprefixer //
+
+autoprefixer: {
+      development: {
+        browsers: ['last 2 version', 'ie 9'],
+        expand: true,
+        flatten: true,
+        src: 'css/*.css',
+        dest: 'css/'
+      }
+    },
+
+//End Autoprefixer //
+
+
+// Grunt Watch //
     watch: {
       grunt: { files: ['Gruntfile.js'] },
 
@@ -24,12 +43,18 @@ module.exports = function(grunt) {
         tasks: ['sass']
       }
     }
-  });
+// End Grunt Watch //  
   
+  });
+
+// Define Grunt Tasks //
+
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-reload');
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('build', ['sass', 'autoprefixer']);
   grunt.registerTask('default', ['build','watch']);
-  grunt.loadNpmTasks('grunt-reload');
+
 }
