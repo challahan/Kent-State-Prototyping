921dc5cf66bf886003170caa3de6cfab7e46b2da
Christopher Hallahan <challahan@gmail.com>
2014-03-07 13:57:41 -0500

    Added to Research landing page and added new ScrollTo plugin
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

---
+++ js/scroll.js
@@ -0,0 +1,15 @@
+
+      $(function() {
+  $('a[href*=#]:not([href=#panel2-1],[href=#panel2-2],[href=#panel2-3],[href=#panel2-4])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
