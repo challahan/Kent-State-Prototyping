ad56c93c64c1a3da5ffc487f4b82895d1bea45db
Christopher Hallahan <challahan@gmail.com>
2014-04-02 00:20:36 -0400

    Compressed Image Files Added
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,6 +37,47 @@ autoprefixer: {
 //End Autoprefixer //
 
 
+// Image Compression //
+
+
+imagemin: {
+    png: {
+      options: {
+        optimizationLevel: 7
+      },
+      files: [
+        {
+          // Set to true to enable the following options…
+          expand: true,
+          // cwd is 'current working directory'
+          cwd: 'img/',
+          src: ['**/*.png'],
+          // Could also match cwd line above. i.e. project-directory/img/
+          dest: 'img/compressed/',
+          ext: '.png'
+        }
+      ]
+    },
+    jpg: {
+      options: {
+        progressive: true
+      },
+      files: [
+        {
+          // Set to true to enable the following options…
+          expand: true,
+          // cwd is 'current working directory'
+          cwd: 'img/',
+          src: ['**/*.jpg'],
+          // Could also match cwd. i.e. project-directory/img/
+          dest: 'img/compressed/',
+          ext: '.jpg'
+        }
+      ]
+    }
+  },
+
+
 // Grunt Watch //
     watch: {
       grunt: { files: ['Gruntfile.js'] },
@@ -53,11 +94,12 @@ autoprefixer: {
 // Define Grunt Tasks //
 
   grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-reload');
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('build', ['autoprefixer', 'sass']);
-  grunt.registerTask('default', ['build','watch']);
+  grunt.registerTask('default', ['imagemin','build','watch']);
 
 }
