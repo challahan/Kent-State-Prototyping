7257a2f5a134b41b3a38799368cd2835090df167
Christopher Hallahan <challahan@gmail.com>
2014-06-19 15:25:13 -0400

    Added profiles
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,6 @@
 
       $(function() {
-  $('a[href*=#]:not([href=#panel2-1],[href=#panel2-2],[href=#panel2-3],[href=#panel2-4],[href=#panel1],[href=#panel2],[href=#panel3],[href=#panel1a],[href=#panel2a],[href=#panel3a],[href=#panel4a],[href=#panel5a])').click(function() {
+  $('a[href*=#]:not([href=#panel2-1],[href=#panel2-2],[href=#panel2-3],[href=#panel2-4],[href=#panel1],[href=#panel2],[href=#panel3], ,[href=#panel4],[href=#panel5],[href=#panel1a],[href=#panel2a],[href=#panel3a],[href=#panel4a],[href=#panel5a])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
